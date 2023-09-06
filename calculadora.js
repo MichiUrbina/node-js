2357e737c01357f46d830259c5a53fa32b6125df
MichiUrbina <138550522+MichiUrbina@users.noreply.github.com>
2023-09-05 20:44:00 -0600

    calculadora

---
+++ calculadora.js
@@ -0,0 +1,8 @@
+const {suma, rest, mult, div } = require("./operaciones")
+
+
+
+console.log(suma(12, 64));
+console.log(rest(12, 64));
+console.log(mult(12, 64));
+console.log(div(12, 64));
---
+++ index.js
@@ -0,0 +1,2 @@
+let numero = 12345
+let numero2 = 324
